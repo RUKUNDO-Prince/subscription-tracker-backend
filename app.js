b0487ce3daf64415a73dd5f799435130b45c101a
RUKUNDO Prince <rukundoprince951@gmail.com>
2025-04-18 13:31:23 +0200

    finished workflows and email reminders

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ import subscriptionRouter from './routes/subscription.routes.js';
 
 import connectToDatabase from './database/mongodb.js';
 import errorMiddleware from './middlewares/error.middleware.js';
-import arcjetMiddleware from './middlewares/arcjet.middleware.js';
+import workflowRouter from './routes/workflow.routes.js';
+// import arcjetMiddleware from './middlewares/arcjet.middleware.js';
 
 const app = express();
 
@@ -21,6 +22,7 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
 
 app.use(errorMiddleware);
 
